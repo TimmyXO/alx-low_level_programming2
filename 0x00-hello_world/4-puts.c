2256587ba16faf480133dd9b9d97e7d31566752a
TimmyXO <titzkid@gmail.com>
2022-12-11 12:00:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
-
-* main - Prints quote
-
-*
-
-* Return: zero on success
-
-*
-
-*/
-
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
-
-{
-    
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return (0);
-    
+{  puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
