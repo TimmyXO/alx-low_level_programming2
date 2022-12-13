dfefb2d0e2ee9d8d1a566695dc4787ddeb4abf87
TimmyXO <titzkid@gmail.com>
2022-12-13 14:21:18 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 #include "6-abs.c"
 /**
- * _abs - The absolute value for an interger
- *
- * @n: interger to be tested
- *
- * Return: 0 or 1
- */
-int _abs(int n) 
+* print_last_digit - main function
+*
+* @n: integer to get last digit of
+* Return: last digit of n
+*
+*/
+int print_last_digit(int n)
 {
-return (n < 0 ? -1 * n : n);
+_putchar('0' + _abs(n % 10));  
+return (_abs(n % 10));
 }
