e8b36a39d7676aac079dbeab5bf1611ba8f407d8
TimmyXO <titzkid@gmail.com>
2022-12-13 21:54:03 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 10-main.c 10-add.c -o 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - The function that adds two integers and returns the result
+*
+* @a: int type number
+*
+* @b: int type number
+*
+* Return: 0
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
